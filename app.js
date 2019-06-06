7910d0d897bcaebbeb78061c4fd9f68bec83199f
Levin Winter <33220502+levinwinter@users.noreply.github.com>
2019-06-06 16:54:54 +0200

    removed liveClients Array

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const io = require('socket.io')(http);
 const port = 80;
 
 const connectedUsers = new Map();
-const liveClients = [];
 
 app.set('view engine', 'pug');
 
@@ -18,19 +17,18 @@ app.get('/user/:username', (req, res) => { res.render('user'); });
 app.get('/live', (req, res) => { res.render('live'); });
 app.get('/manage', (req, res) => { res.render('manage'); });
 
+io.use((socket, next) => {
+  if (socket.handshake.query.type === 'user') {
+    connectedUsers.set(socket.id, socket.handshake.query.name);
+    io.emit('userUpdate', Array.from(connectedUsers));
+  }
+  return next();
+});
+
 io.on('connection', (socket) => {
-  socket.on('user', (msg) => {
-    io.emit('user', msg);
-  });
-
-  socket.on('buzzed', (id) => {
-    io.emit('buzzed', id);
-  });
-
-  socket.on('reset', () => {
-    console.log('reset');
-    io.emit('reset');
-  });
+  socket.on('user', (msg) => { io.emit('user', msg); });
+  socket.on('buzzed', (id) => { io.emit('buzzed', id); });
+  socket.on('reset', () => { io.emit('reset'); });
 
   socket.on('disconnect', () => {
     connectedUsers.delete(socket.id);
@@ -38,23 +36,4 @@ io.on('connection', (socket) => {
   });
 });
 
-io.use((socket, next) => {
-  if (socket.handshake.query.type === 'live') {
-    liveClients.push(socket);
-    return next();
-  }
-  if (socket.handshake.query.type === 'user') {
-    connectedUsers.set(socket.id, socket.handshake.query.name);
-    connectedUsers.forEach((value, key) => {
-      console.log(`${key} = ${value}`);
-    });
-    console.log('============');
-    io.emit('userUpdate', Array.from(connectedUsers));
-    return next();
-  }
-  return next();
-});
-
-http.listen(port, () => {
-  console.log(`quiz.io is up and running at localhost: ${port}`);
-});
+http.listen(port);
