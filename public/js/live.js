5fabafe1224a4aed92184cf7e7a026bf9e9e7a1c
Levin Winter <33220502+levinwinter@users.noreply.github.com>
2019-06-15 11:40:59 +0200

    replaced live with sandbox feature

--- public/js/live.js
+++ public/js/live.js
@@ -1,36 +1,93 @@
-const socket = io({ query: { type: 'live' } });
+const gray = 'rgb(238, 238, 238)';
+const red = 'rgb(255, 0, 0)';
 
-let locked = false;
-let connectedUsers = new Map();
-let lastBuzzed;
+// eslint-disable-next-line no-undef
+const socket = io({ query: { type: 'sandbox' } });
 
-socket.emit('getUsers');
-
-function generateUserHTML(id, name) {
-  return `<div class="level-item has-text-centered is-marginless" id=${id}><p class="title is-1">${name}</p></div>`;
+function setScore(player, score) {
+  $('#overlay').contents().find(`#point7.${player}`).hide();
+  $('#overlay').contents().find(`#point6.${player}`).hide();
+  $('#overlay').contents().find(`#point5.${player}`).hide();
+  $('#overlay').contents().find(`#point4.${player}`).hide();
+  $('#overlay').contents().find(`#point3.${player}`).hide();
+  $('#overlay').contents().find(`#point2.${player}`).hide();
+  $('#overlay').contents().find(`#point1.${player}`).hide();
+  switch (score) {
+    case 7:
+      $('#overlay').contents().find(`#point7.${player}`).show();
+      // falls through
+    case 6:
+      $('#overlay').contents().find(`#point6.${player}`).show();
+      // falls through
+    case 5:
+      $('#overlay').contents().find(`#point5.${player}`).show();
+      // falls through
+    case 4:
+      $('#overlay').contents().find(`#point4.${player}`).show();
+      // falls through
+    case 3:
+      $('#overlay').contents().find(`#point3.${player}`).show();
+      // falls through
+    case 2:
+      $('#overlay').contents().find(`#point2.${player}`).show();
+      // falls through
+    case 1:
+      $('#overlay').contents().find(`#point1.${player}`).show();
+      // no default
+  }
 }
 
-function updateUsers(userArray) {
-  connectedUsers = new Map(userArray);
-  $('.level').empty();
-  connectedUsers.forEach((value, key) => {
-    $('.level').append(generateUserHTML(key, value));
-  });
+function setTimer(player, time) {
+  $('#overlay').contents().find(`#time5.${player}`).css('fill', gray);
+  $('#overlay').contents().find(`#time4.${player}`).css('fill', gray);
+  $('#overlay').contents().find(`#time3.${player}`).css('fill', gray);
+  $('#overlay').contents().find(`#time2.${player}`).css('fill', gray);
+  $('#overlay').contents().find(`#time1.${player}`).css('fill', gray);
+  switch (time) {
+    case 5:
+      $('#overlay').contents().find(`#time5.${player}`).css('fill', red);
+      $('#overlay').contents().find(`#time4.${player}`).css('fill', red);
+      $('#overlay').contents().find(`#time3.${player}`).css('fill', red);
+      $('#overlay').contents().find(`#time2.${player}`).css('fill', red);
+      $('#overlay').contents().find(`#time1.${player}`).css('fill', red);
+      break;
+    case 4:
+      $('#overlay').contents().find(`#time4.${player}`).hide();
+      // falls through
+    case 3:
+      $('#overlay').contents().find(`#time3.${player}`).hide();
+      // falls through
+    case 2:
+      $('#overlay').contents().find(`#time2.${player}`).hide();
+      // falls through
+    case 1:
+      $('#overlay').contents().find(`#time1.${player}`).hide();
+      // no default
+  }
 }
 
-socket.on('buzzed', (id) => {
-  if (!locked) {
-    locked = true;
-    $(`#${id}`).addClass('has-background-success');
-    lastBuzzed = id;
+socket.on('score', (msg) => {
+  setScore(msg.name, msg.score);
+});
+
+socket.on('live-view', (view) => {
+  console.log(view);
+  $('#overlay').hide();
+  $('#status').hide();
+  switch (view) {
+    case 'overlay':
+      $('#overlay').show();
+      break;
+    case 'status':
+      $('#status').show();
+      break;
+      // no default
   }
 });
 
-socket.on('getUsers', (userArray) => { updateUsers(userArray); });
-
-socket.on('userUpdate', (userArray) => { updateUsers(userArray); });
-
-socket.on('reset', () => {
-  locked = false;
-  $(`#${lastBuzzed}`).removeClass('has-background-success');
+$(document).ready(() => {
+  setTimer('Schacht', 5);
+  setScore('Schacht', 3);
+  setTimer('Lehrer', 3);
+  setScore('Lehrer', 6);
 });
